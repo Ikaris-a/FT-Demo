bfae02df3a703237e74d68aa2d7648139bb511dc
Ikaris-a <240570941@qq.com>
2023-03-31 16:05:14 +0800

    test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">hello {{ test }}</div>
+</template>
+
+<script>
+import { addressesByNetwork, SupportedChainId } from '@looksrare/sdk';
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      test: 1,
+    };
+  },
+  created() {
+    this.testFun();
+  },
+  methods: {
+    async testFun() {
+      const addresses = await addressesByNetwork[SupportedChainId.MAINNET];
+      console.log(addresses);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
